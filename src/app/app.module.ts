ede97dfe9332ebfe258ac24d3083a60ab2ee5bef
Kadweka <husseinkatana17@gmail.com>
2021-12-25 23:21:16 +0300

    upddate the products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgxStarRatingModule } from 'ngx-star-rating';
 import { LayoutModule } from './layout/layout.module';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
 import { TokenInterceptor } from './core/interceptors/token.interceptor';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { ProductDetailsComponent } from './views/product-details/product-details.component';
 import { CategoriesComponent } from './views/categories/categories.component';
 import { AddedCartComponent } from './views/added-cart/added-cart.component';
-import { RatingsComponent } from './views/product-details/ratings/ratings.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductDetailsComponent,
     CategoriesComponent,
     AddedCartComponent,
-    RatingsComponent
   ],
   imports: [
     BrowserModule,
